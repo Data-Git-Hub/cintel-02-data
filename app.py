4af56002babfd25241ea6760b6f6f3b902e64704
Data Analytics Graduate Student <s576977@nwmissouri.edu>
2024-10-23 17:45:52 -0500

    Update app.py
    Version 1.0
    - added import line for dataset.
    -Changed name of title on Output Object.

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 import plotly.express as px
 from shiny.express import input, ui
 from shinywidgets import render_plotly
+# (1.0) This package provides the Palmer Penguins dataset
+import palmerpenguins
 
-ui.page_opts(title="Filling layout", fillable=True)
+# (1.0) Use the built-in function to load the Palmer Penguins dataset
+penguins_df = palmerpenguins.load_penguins()
+
+ui.page_opts(title="Penguins are Cool", fillable=True)
 with ui.layout_columns():
 
     @render_plotly
